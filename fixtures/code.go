e71ea0f9da13b77e31c46f05601d00634d08d8dc
Ayooluwa Isaiah <ayisaiah@gmail.com>
2024-05-03 12:42:58 +0100

    add fixtures package and test data

---
+++ fixtures/code.go
@@ -0,0 +1,14 @@
+package fixtures
+
+import (
+	"bytes"
+	"encoding/json"
+)
+
+func PrettyPrintJSON(str string) (string, error) {
+	var b bytes.Buffer
+	if err := json.Indent(&b, []byte(str), "", "    "); err != nil {
+		return "", err
+	}
+	return b.String(), nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/betterstack-community/advanced-go-unit-tests
+
+go 1.22.2
